8f565b36d73bd46540691bb52ae68acf5fe11ecf
oussama <o.belfassi7993@uca.ac.ma>
2026-04-14 00:37:50 +0100

    Add debug info to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 PORT=${PORT:-80}
 
+echo "=== DEBUG ==="
+echo "PORT variable: $PORT"
+echo "ports.conf before:"
+cat /etc/apache2/ports.conf
+echo "============="
+
 sed -i "s/Listen 80/Listen $PORT/" /etc/apache2/ports.conf
 sed -i "s/*:80/*:$PORT/" /etc/apache2/sites-available/000-default.conf
 
+echo "ports.conf after:"
+cat /etc/apache2/ports.conf
 echo "Starting Apache on port $PORT"
+
 exec apache2-foreground
