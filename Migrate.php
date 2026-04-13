8bd988317544f9adebd9e8758e3ee2ad474a9fbf
oussama <o.belfassi7993@uca.ac.ma>
2026-04-13 13:15:22 +0100

    Make some Modifications

--- Migrate.php
+++ Migrate.php
@@ -9,9 +9,9 @@ $dotenv = Dotenv\Dotenv::createUnsafeMutable(__DIR__);
 $dotenv->safeLoad();
 
 $config = [
-    'dsn'      => $_ENV['DB_DSN']      ?? getenv('DB_DSN'),
-    'user'     => $_ENV['DB_USER']     ?? getenv('DB_USER'),
-    'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD'),
+    'dsn'      => $_ENV['DB_DSN']      ?? getenv('DB_DSN')  ?? '',
+    'user'     => $_ENV['DB_USER']     ?? getenv('DB_USER') ?? '',
+    'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?? '',
 ];
 
 $db = new Database($config);
