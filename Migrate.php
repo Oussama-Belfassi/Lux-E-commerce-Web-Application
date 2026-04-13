24e86342557aa5e87d398405225d143541c5ecba
Oussama Belfassi <o.belfassi7993@uca.ac.ma>
2026-04-13 11:49:08 +0100

    Refactor dotenv loading and enhance DB config fallback
    Updated dotenv loading method to use safeLoad and added fallback to getenv for database configuration.

--- Migrate.php
+++ Migrate.php
@@ -4,13 +4,13 @@ use app\Database;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv = Dotenv\Dotenv::createUnsafeMutable(__DIR__);
+$dotenv->safeLoad();
 
 $config = [
-    'dsn'      => $_ENV['DB_DSN'],
-    'user'     => $_ENV['DB_USER'],
-    'password' => $_ENV['DB_PASSWORD'],
+    'dsn'      => $_ENV['DB_DSN']      ?? getenv('DB_DSN'),
+    'user'     => $_ENV['DB_USER']     ?? getenv('DB_USER'),
+    'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD'),
 ];
 
 $db = new Database($config);
